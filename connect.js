579d0095e3ab5ec4a64c100d35b30992a8f43850
shohan494 <shohan12224943@gmail.com>
2017-11-02 10:01:49 +0600

    installed mysql package

---
+++ connect.js
@@ -0,0 +1,13 @@
+
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodecrud'
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
