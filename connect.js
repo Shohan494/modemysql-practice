e32a07630ea7c855fda127a19e59d23cae4d8944
shohan494 <shohan12224943@gmail.com>
2017-11-02 10:11:48 +0600

    query using mysql works

--- connect.js
+++ connect.js
@@ -7,7 +7,20 @@ const connection = mysql.createConnection({
   password: '',
   database: 'nodecrud'
 });
+
 connection.connect((err) => {
   if (err) throw err;
   console.log('Connected!');
 });
+
+connection.query('SELECT * FROM employees', (err,rows) => {
+  if(err) throw err;
+
+  console.log('Data received from Db:\n');
+  console.log(rows);
+
+  rows.forEach( (row) => { 
+  console.log(`${row.name} is in ${row.location}`); 
+});
+});
+
