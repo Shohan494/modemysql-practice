c859001b10afa3e953b767e90c17cc81def1e309
shohan494 <shohan12224943@gmail.com>
2017-11-02 14:09:48 +0600

    express with mysql basic done

--- expressmysql.js
+++ expressmysql.js
@@ -17,15 +17,20 @@ connection.connect((err) => {
 
 app.set('port', 3000);
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', function(req, res){
   connection.query('SELECT * FROM employees', function(err, rows){
-    res.render('employees', {employees : rows});
+    res.render('index', {title: 'Express MySql Node App', rows: JSON.stringify(rows)});
   });
 });
 
+/*app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+*/
 
 app.listen(app.get('port'));
 console.log('Express server listening on port ' + app.get('port'));
